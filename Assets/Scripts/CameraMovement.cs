e2e68c9a4f08e6bab9c8fc305d1aa9f48dbeefc7
LucasViana18 <lucasvviana14@gmail.com>
2019-11-28 22:14:22 +0000

    First commit - player and camera movement as well as some light experiments

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Instance variables
+    [SerializeField] private float sensibility = 100f;
+    [SerializeField] private Transform body;
+    private float verticalRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        // Local variables
+        float mouseX;
+        float mouseY;
+
+        // Mouse input
+        mouseX = Input.GetAxis("Mouse X") * sensibility * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensibility * Time.deltaTime;
+
+        // Process of calculation and clamp of the x axis rotation of camera
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        // Rotation of the camera or player
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        body.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Instance variables
+    [SerializeField] private float moveSpeed = 20f;
+    private CharacterController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // Local variables
+        float moveX;
+        float moveZ;
+        Vector3 move;
+
+        // Movement input
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
+
+        // Calculation of movement
+        move = transform.right * moveX + transform.forward * moveZ;
+
+        // Action of movement to the character controller
+        controller.Move(Vector3.ClampMagnitude(move, 1f) * moveSpeed * Time.deltaTime);
+    }
+}
