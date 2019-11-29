716d10336f2146b3107f220cfcd8e5e8527f0abf
Rita Saraiva <belgas2000@gmail.com>
2019-11-29 12:45:53 +0000

    Implement gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,13 @@ public class Player : MonoBehaviour
 {
     // Instance variables
     [SerializeField] private float moveSpeed = 20f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private LayerMask checkMask;
+    [SerializeField] private float checkRadius = 0.5f;
+    [SerializeField] private Transform checkBody;
     private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -20,6 +26,11 @@ public class Player : MonoBehaviour
         float moveZ;
         Vector3 move;
 
+        isGrounded = Physics.CheckSphere(checkBody.position, checkRadius, checkMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
         // Movement input
         moveX = Input.GetAxis("Horizontal");
         moveZ = Input.GetAxis("Vertical");
@@ -29,5 +40,9 @@ public class Player : MonoBehaviour
 
         // Action of movement to the character controller
         controller.Move(Vector3.ClampMagnitude(move, 1f) * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
