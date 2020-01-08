d761a1cc96377504d2d3c1448e5cd66ff13ae36a
LucasViana18 <lucasvviana14@gmail.com>
2020-01-08 20:51:47 +0000

    Hide mouse cursor

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -12,6 +10,7 @@ public class CameraMovement : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
