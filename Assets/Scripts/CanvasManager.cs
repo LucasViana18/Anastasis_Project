739a6633534780b31e84df0cc5e24a6cd0ed8686
Lucas Viana <a21805095@alunos.ulht.pt>
2020-01-10 18:23:56 +0000

    Fix project pt3

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject   interactionPanel;
+    public Text         interactionText;
+    public GameObject bodyAmuletPanel;
+    public PlayerInteractions playerinter;
+
+    public void Start()
+    {
+        HideInteractionPanel();
+    }
+
+    public void Update()
+    {
+        if (playerinter.ConfirmAmulet || Input.GetKeyDown(KeyCode.C))
+            ShowAmuletPanel();
+    }
+
+    public void ShowInteractionPanel(string interactionMessage)
+    {
+        interactionText.text = interactionMessage;
+        interactionPanel.SetActive(true);
+    }
+
+    public void HideInteractionPanel()
+    {
+        interactionPanel.SetActive(false);
+    }
+
+    public void ShowAmuletPanel()
+    {
+        bodyAmuletPanel.SetActive(true);
+    }
+
+    public void HideAmuletPanel()
+    {
+        bodyAmuletPanel.SetActive(false);
+    }
+}
