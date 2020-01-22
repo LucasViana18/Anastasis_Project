0420baa8d965cbf4a2f01918c9561f4c7aee1ee2
Rita Simões Saraiva <belgas2000@gmail.com>
2020-01-22 02:04:10 +0000

    Pause Menu

--- Assets/Scripts/PickUpItems.cs
+++ Assets/Scripts/PickUpItems.cs
@@ -9,28 +9,31 @@ public class PickUpItems : MonoBehaviour
     private Vector3 postLastFame;
     [SerializeField] private Player player;
     [SerializeField] private GameObject spinPanel;
+    [SerializeField] private Transform camera;
 
     private void Update()
     {
-
-        if (Input.GetMouseButtonDown(0))
+        if (Physics.Raycast(camera.position, camera.forward, out RaycastHit hit, 4f))
         {
-            spinPanel.SetActive(true);
-            player.enabled = false;
-            GetComponent<Rigidbody>().useGravity = false;
-            this.transform.position = Destination.position;
-            this.transform.parent = GameObject.Find("Destination").transform;
+            if (Input.GetMouseButtonDown(0))
+            {
+                spinPanel.SetActive(true);
+                player.enabled = false;
+                GetComponent<Rigidbody>().useGravity = false;
+                this.transform.position = Destination.position;
+                this.transform.parent = GameObject.Find("Destination").transform;
+            }
+
+            if (Input.GetMouseButton(1))
+            {
+                var delta = Input.mousePosition - postLastFame;
+
+                var axis = Quaternion.AngleAxis(-90f, Vector3.forward) * delta;
+                transform.rotation = Quaternion.AngleAxis(delta.magnitude * 0.002f, axis) * transform.rotation;
+
+            }
         }
-
-        if (Input.GetMouseButton(1))
-        {
-            var delta = Input.mousePosition - postLastFame;
-
-            var axis = Quaternion.AngleAxis(-90f, Vector3.forward) * delta;
-            transform.rotation = Quaternion.AngleAxis(delta.magnitude * 0.002f, axis) * transform.rotation;
-
-        }
-
+        
         if (Input.GetKeyDown("space"))
         {
             this.transform.parent = null;
