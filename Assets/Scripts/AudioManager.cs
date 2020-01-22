073e998319e1c39d0618fb08b55ea579d1383236
LucasViana18 <lucasvviana14@gmail.com>
2020-01-22 23:25:21 +0000

    Add audio setup

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Sound[] sounds;
+    private void Start()
+    {
+        // Goes through every clip and adds to the new audio source
+        foreach(Sound s in sounds)
+        {
+            s.Source = gameObject.AddComponent<AudioSource>();
+            s.Source.clip = s.Audio;
+
+            s.Source.volume = s.Volume;
+        }
+    }
+
+    public void Play(string name)
+    {
+        //Plays the sound
+        Sound s = Array.Find(sounds, sound => sound.Name == name);
+        if (s == null) return;
+        s.Source.Play();
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    // Name
+    [SerializeField] private string name;
+    public string Name { get => name; }
+
+    // Audio clip
+    [SerializeField] private AudioClip audio;
+    public AudioClip Audio { get => audio; }
+
+    // Volume
+    [Range(0f, 1f)]
+    [SerializeField] private float volume;
+    public float Volume { get => volume; }
+
+    // Audio Source
+    [HideInInspector]
+    public AudioSource Source { get; set; }
+}
